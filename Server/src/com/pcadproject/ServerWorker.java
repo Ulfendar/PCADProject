fe30d2963616e50ba0f2a474e08b29607bb9c786
Ulfendar <riccardo.didio@gmail.com>
2019-03-27 21:57:02 +0100

    creare, joinare e lasciare un gruppo di messaggi

--- Server/src/com/pcadproject/ServerWorker.java
+++ Server/src/com/pcadproject/ServerWorker.java
@@ -6,6 +6,7 @@ import org.apache.commons.lang3.StringUtils;
 import java.io.*;
 import java.net.Socket;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
 
 public class ServerWorker extends Thread {
@@ -13,11 +14,13 @@ public class ServerWorker extends Thread {
     private Server server;//contiene la lista degli altri ServerWorkers
     private String login = null;
     private OutputStream outputStream;
+    HashSet<String> topicSet;
 
     public ServerWorker(Server server, Socket clientSocket) throws IOException {
         this.server = server;
         this.clientSocket = clientSocket;
         this.outputStream = clientSocket.getOutputStream();
+        this.topicSet = new HashSet<>();
     }
 
     public String getLogin() {
@@ -44,7 +47,7 @@ public class ServerWorker extends Thread {
         try (BufferedReader reader = new BufferedReader(
                 new InputStreamReader(inputStream))) {
             while ((line = reader.readLine()) != null) {
-                String[] tokens = StringUtils.split(line);
+                String[] tokens = StringUtils.split(line,null, 3);
 
                 if (tokens != null && tokens.length > 0) {
                     String cmd = tokens[0];
@@ -52,8 +55,11 @@ public class ServerWorker extends Thread {
                     if ("quit".equalsIgnoreCase(cmd)) {
                         handleLogOut();
                         break;
-                    } else if ("login".equalsIgnoreCase(cmd)) handleLogin(outputStream, tokens);
-                    else if ("msg".equalsIgnoreCase(cmd)) handleMessages(outputStream, tokens);
+                    }
+                    else if ("login".equalsIgnoreCase(cmd)) handleLogin(tokens);
+                    else if ("msg".equalsIgnoreCase(cmd)) handleMessages(tokens);
+                    else if ("join".equalsIgnoreCase(cmd)) handleJoin(tokens);
+                    else if ("leave".equalsIgnoreCase(cmd)) handleLeave(tokens);
                     else {
                         String msg = "Comando non riconosciuto: " + cmd + " \n";
                         send(msg);
@@ -66,24 +72,75 @@ public class ServerWorker extends Thread {
         }
     }
 
+    private void handleLeave(String[] tokens) throws IOException {
+        if (tokens.length >1){
+            String topic = tokens[1];
+            topicSet.remove(topic);
+            send("Uscito da "+topic+"\n");
+        }
+    }
+
+    private boolean isMemberOfTopic(String topic) {
+
+        return topicSet.contains(topic);
+    }
+
+    private void handleJoin(String[] tokens) throws IOException {
+        if (tokens.length >1){
+            String topic = tokens[1];
+            topicSet.add(topic);
+            send(topic+" Joinato\n");
+        }
+    }
+
     private void handleLogOut() throws IOException {
-        send("Arrivederci");
-        String onlineNotification = "L'utente " + login +" e' ora offline!\n";
-        broadcast(onlineNotification);
+        send("Arrivederci\n");
+        String offlineNotification = "L'utente " + login +" e' ora offline!\n";
+        broadcast(offlineNotification);
         server.releaseWorker(this);
         return;
     }
 
-    private void handleMessages(OutputStream outputStream, String[] tokens) throws IOException {
-        String msg = null;
-        if (tokens.length == 2)
-            msg = "Messaggio: " + tokens[1] + "\n";
-        else
-            msg = "Errore di sintassi \n      Prova -> [msg CONTENUTO_MSG]";
-        outputStream.write(msg.getBytes());
+    private void handleMessages(String[] tokens) throws IOException {
+        String msg= "";
+        if ((tokens.length < 3 || tokens[1].equals(null))){
+            msg = "Errore di sintassi \n      Prova -> [msg USERNAME CONTENUTO_MSG]\n";
+            send(msg);
+            return;
+        }
+        String sendTo = tokens[1];
+
+        boolean isTopic = (sendTo.charAt(0) == '@');//starting character for a group
+        boolean isFound = false;
+
+        if(isTopic) msg += ""+ sendTo+":";
+
+        for(ServerWorker worker: server.getServerWorkersList()) {
+            if(isTopic){ //if sendTo is a groupChat...
+                if(worker.isMemberOfTopic(sendTo)) {
+                    isFound = true;
+                    deliverMessage(tokens[2],msg,worker);
+                }
+            }
+            else if(sendTo.equals(worker.getLogin())) {//if user found
+                isFound =true;
+                deliverMessage(tokens[2], msg, worker);
+            }
+
+        }
+        if(isFound == false) {
+            msg = "Utente o Gruppo non trovato!\n";
+            send(msg);
+        }
+
     }
 
-    private void handleLogin(OutputStream outputStream, String[] tokens) throws IOException {
+    private void deliverMessage(String token, String msg, ServerWorker worker) throws IOException {
+        msg += token;
+        worker.send(" " + login + ":\n   " + msg + "\n");
+    }
+
+    private void handleLogin(String[] tokens) throws IOException {
 
         if (tokens.length == 3) {
 
@@ -124,7 +181,7 @@ public class ServerWorker extends Thread {
     private void broadcast(String msg) throws IOException {
 
         for(ServerWorker worker: server.getServerWorkersList()) {
-            worker.send(msg);
+            if(!login.equals(worker.getLogin())) worker.send(msg);
         }
 
     }
